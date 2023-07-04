00f457ca58966ce3aaaf2966f616163199830948
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-03 22:27:21 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -9,4 +9,19 @@ spark = SparkContext.getOrCreate()
 
 spark.addPyFile(SparkFiles.get("/home/kat_wadhwani/BigDataClass/access.log"))
 
-ps.read_csv(spark)
+df = spark.createDataFrame([
+    Row(a=1, b=4., c='GFG1', d=date(2000, 8, 1),
+        e=datetime(2000, 8, 1, 12, 0)),
+   
+    Row(a=2, b=8., c='GFG2', d=date(2000, 6, 2),
+        e=datetime(2000, 6, 2, 12, 0)),
+   
+    Row(a=4, b=5., c='GFG3', d=date(2000, 5, 3),
+        e=datetime(2000, 5, 3, 12, 0))
+])
+ 
+# show table
+df.show()
+ 
+# show schema
+df.printSchema()
