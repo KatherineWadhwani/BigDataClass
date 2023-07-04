8442238ae2cad67a95d125972fd59b39cc7ee981
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-03 22:49:34 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -17,7 +17,15 @@ spark.addPyFile(SparkFiles.get("/home/kat_wadhwani/BigDataClass/access.log"))
 
 sqlContext = SQLContext(spark)
 
-df = sqlContext.createDataFrame([spark.map(row_parse_function).filter("_")
+df = sqlContext.createDataFrame([
+ Row(a=1, b=4., c='GFG1', d=date(2000, 8, 1),
+        e=datetime(2000, 8, 1, 12, 0)),
+   
+    Row(a=2, b=8., c='GFG2', d=date(2000, 6, 2),
+        e=datetime(2000, 6, 2, 12, 0)),
+   
+    Row(a=4, b=5., c='GFG3', d=date(2000, 5, 3),
+        e=datetime(2000, 5, 3, 12, 0))
 ])
  
  
