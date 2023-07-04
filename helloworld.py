4f7eef129ecc8cf93a646c699941f9d987b8ff2f
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-03 18:35:41 -0700

    Update helloworld.py
    adding import

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
+from pyspark import SparkConf, SparkContext
+
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("My app")
+         .set("spark.executor.memory", "1g"))
+sc = SparkContext(conf = conf)
+
 
 logData = access(logFile).cache()
 errors = logData.filter(lambda line: "ERROR" in line)
