e2b807e392866a0ac8d0fa7b0ba2e6ebcee8fcb5
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-04 19:28:30 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -13,7 +13,7 @@ from pyspark.sql import Row
 from pyspark.sql import SparkSession
 
 
-session = SparkSession.builder().appName("Spark Hive Example").enableHiveSupport().getOrCreate()
+session = SparkSession.enableHiveSupport().getOrCreate()
 
 
 spark = SparkContext.getOrCreate()
