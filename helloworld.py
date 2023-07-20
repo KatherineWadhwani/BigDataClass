29cfbb161a8a8dc90aef579e94137ea7206c9c55
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-20 14:59:14 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -38,7 +38,7 @@ def clean_text(text):
 def calculateIDF(term):
     count = 0
     for x in range(10):
-        if dictArray[x].ContainsKey(term):
+        if term in dictArray[x]:
             count+=1
         
     math.log2(10/count)
