21c93b8205c29970abd40f775263e60fae4e4d26
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-19 23:19:01 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -30,7 +30,7 @@ session = SparkSession \
 
 spark = SparkContext.getOrCreate()
 sqlContext = SQLContext(spark)
-spark.addPyFile(SparkFiles.get("/home/kat_wadhwani/nltk_data/corpora/inagural"))
+spark.addPyFile(SparkFiles.get("/home/kat_wadhwani/nltk_data/corpora/inaugural"))
 
 
 
