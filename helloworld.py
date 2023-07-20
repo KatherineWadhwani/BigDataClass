67de1330a64dd648efb2cea6ccf914edd1266c33
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-20 12:16:10 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -18,7 +18,7 @@ import re
 def clean_text(text):
      text = text.lower()
      text = re.sub('\[.*?\]', '', text)
-     text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+     text = re.sub('[%s]' % re.escape(str.punctuation), ' ', text)
      text = re.sub('[\d\n]', ' ', text)
      return text
 
