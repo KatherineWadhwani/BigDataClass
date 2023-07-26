aa6543c901b5685403dd09562c7058bae00778ea
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-26 16:34:25 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
            
             
             #Print stream
-            goog.pprint()
+            google.pprint()
             
             #Run
             ssc.start()
