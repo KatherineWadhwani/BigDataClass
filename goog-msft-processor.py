c2c65015b228746d1eb21b2354437a1392da0594
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-26 16:13:24 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -19,13 +19,18 @@ if __name__ == "__main__":
 #Setup          
             sc = SparkContext(appName="Proj7")
             ssc = StreamingContext(sc, 1)
+
+
+            String[] nameArrays = new String[] { “date”, “google”, “msft”};
             
             #Create stream on port 9999 on localhost  
             text_stream =  ssc.socketTextStream("localhost", 9999)
             
             #Create new stream off of previous steram (e.g. preform transformation)
             google = text_stream.flatMap(lambda line: line.split (" "))\
-                        .map(lambda word: (word, 1))\
+                        .map(lambda name: (name, 1))\
+                        .map(lambda google: (name, 2))\
+                        .map(lambda msft: (name, 3))
             
             
             #Assignment-specific
