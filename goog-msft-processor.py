8d369bd2f6fb4f37eef4e5dcc68f97b1a4fa3b90
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-26 17:24:29 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
             
             #Create new stream off of previous steram (e.g. preform transformation)
             google = text_stream.flatMap(lambda line: line.split (" "))\
-                        .filter(lambda x: '-' in x)
+                        .range(0, 3)\
+                        .filter(i -> i % 2 == 0)
 
             #goog = google.filter(lambda x: x % 6 != 0)
             
