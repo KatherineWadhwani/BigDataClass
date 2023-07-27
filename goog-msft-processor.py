da8a78b6ecdcbe808ad82d8ef50b0ead0c927aee
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-27 14:59:29 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
             goog40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
                                       .window(40, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
-                                      .filter(lambda x: x[2] == 10)\
+                                      .filter(lambda x: x[2] == 40)\
                                       .map(lambda line: (line[0], line[1]/10, line[2]))
 
             msft10Day = googPrice.map(lambda line: (line[0], line[1], 1))\
@@ -53,12 +53,18 @@ if __name__ == "__main__":
             msft40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
                                       .window(40, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1]/10, a[2] + b[2]))\
-                                      .filter(lambda x: x[2] == 10)\
+                                      .filter(lambda x: x[2] == 40)\
                                       .map(lambda line: (line[0], line[1]/10, line[2]))
 
-        
+
+
+            #Join Streams to Generate Signals
+            signalGoogle = goog10Day.join(goog40Day)\
+                                    .map(lambda x: (x[0], x[1][0], x[1][1], x[2]))
+
+            
             #Print stream
-            googPrice.pprint()
+            signalGoogle.pprint()
             
             #Run
             ssc.start()
