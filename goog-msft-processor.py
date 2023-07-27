3089b400b25a9ebc5f7fc18a4bb7a7eb7ac58b10
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-27 14:44:51 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -37,24 +37,24 @@ if __name__ == "__main__":
                                       .window(10, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
                                       .filter(lambda x: x[2] == 10)\
-                                      .map(lambda line: (line[0], line[1]/10), line[2])
+                                      .map(lambda line: (line[0], line[1]/10, line[2])
 
             goog40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
                                       .window(40, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
                                       .filter(lambda x: x[2] == 10)\
-                                      .map(lambda line: (line[0], line[1]/10), line[2])
+                                      .map(lambda line: (line[0], line[1]/10, line[2])
 
             msft10Day = googPrice.map(lambda line: (line[0], line[1], 1))\
                                       .window(10, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
                                       .filter(lambda x: x[2] == 10)\
-                                      .map(lambda line: (line[0], line[1]/10), line[2])
+                                      .map(lambda line: (line[0], line[1]/10, line[2])
             msft40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
                                       .window(40, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1]/10, a[2] + b[2]))\
                                       .filter(lambda x: x[2] == 10)\
-                                      .map(lambda line: (line[0], line[1]/10), line[2])
+                                      .map(lambda line: (line[0], line[1]/10, line[2])
 
         
             #Print stream
