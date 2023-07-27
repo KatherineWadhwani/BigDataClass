a6755764a5601f05fcefe3f22bd3493f4c3f8547
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-27 14:21:40 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -37,6 +37,18 @@ if __name__ == "__main__":
                                       .window(10, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))
 
+             goog40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(40, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))
+
+             msft10Day = googPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(10, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))
+             msft40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(40, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1]/10, a[2] + b[2]))
+                                      .filter(lambda x: x[2] == 10)
+
         
             #Print stream
             goog10Day.pprint()
