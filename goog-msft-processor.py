c8692bd9590342c1219960ba8b93dad74d047b35
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 00:00:20 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -8,22 +8,10 @@ def findHigherGoog(tenDay, fortyDay):
                         print(topGoog)
                         if (tenDay > fortyDay):
                                     topGoog = "tenDay"
-                                    if (oldTop == topGoog):
-                                                #print("1")
-                                    if (oldTop != topGoog and oldTop == "empty"):
-                                                #print("2")
-                                    if (oldTop != topGoog and oldTop != "empty"):
-                                                #print("buy")
+                                  
                         else:
                                     topGoog = "fortyDay"
-                                    if (oldTop == topGoog):
-                                                #print("3")
-                                    if (oldTop != topGoog and oldTop == "empty"):
-                                                #print("4")
-                                    if (oldTop != topGoog and oldTop != "empty"):
-                                                #print("buy")
                                     
-
 
 
 findHigherGoog(4, 10)
