8a9943e7de0d49307f367aab25e7403818a21d8a
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 00:01:28 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -1,21 +1,31 @@
-topGoog = "empty"
-topMsft = "empty"
 
 
-def findHigherGoog(tenDay, fortyDay):
+            topGoog = "empty"
+            topMsft = "empty"
+
+            def findHigherGoog(tenDay, fortyDay):
                         global topGoog
                         oldTop = topGoog
-                        print(topGoog)
                         if (tenDay > fortyDay):
                                     topGoog = "tenDay"
-                                  
+                                    if (oldTop == topGoog):
+                                                return "1"
+                                    if (oldTop != topGoog and oldTop == "empty"):
+                                                return "2"
+                                    if (oldTop != topGoog and oldTop != "empty"):
+                                                return "buy "
                         else:
                                     topGoog = "fortyDay"
-                                    
+                                    if (oldTop == topGoog):
+                                                return "3"
+                                    if (oldTop != topGoog and oldTop == "empty"):
+                                                return "4"
+                                    if (oldTop != topGoog and oldTop != "empty"):
+                                                return "sell "
 
 
-findHigherGoog(4, 10)
-findHigherGoog(20, 10)
+          findHigherGoog(4, 10)
+          findHigherGoog(20, 10)
 
 
 
