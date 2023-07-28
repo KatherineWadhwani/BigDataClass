0362753dc1e76903385f36c326d5551e1db37299
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 00:02:35 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -1,4 +1,24 @@
+#!/usr/bin/env python3
+import pandas as pd
+import os
+import math
+import string
+import numpy as np
+import re
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark import SparkFiles
+from pyspark.sql import SQLContext
+from pyspark.sql import Row
+from pyspark.sql import SparkSession
+from nltk.corpus.reader.util import StreamBackedCorpusView
+import sys
+import datetime
 
+if __name__ == "__main__":
+#Setup          
+            sc = SparkContext(appName="Proj7")
+            ssc = StreamingContext(sc, 1)
 
             topGoog = "empty"
             topMsft = "empty"
@@ -22,10 +42,61 @@
                                                 return "4"
                                     if (oldTop != topGoog and oldTop != "empty"):
                                                 return "sell "
+                                                
+            def findHigherMsft(tenDay, fortyDay):
+                        global topMsft
+                        oldTop = topMsft   
+                        if (tenDay > fortyDay):
+                                    topMsft = "tenDay"
+                                    if (oldTop == topMsft):
+                                                return "5"
+                                    if (oldTop != topMsft and oldTop == "empty"):
+                                                return "6"
+                                    if (oldTop != topMsft and oldTop != "empty"):
+                                                return "buy "
+                        else:
+                                    topMsft = "fortyDay"
+                                    if (oldTop == topMsft):
+                                                return "7"
+                                    if (oldTop != topMsft and oldTop == "empty"):
+                                                return "8"
+                                    if (oldTop != topMsft and oldTop != "empty"):
+                                                return "sell "
+            
+            #Create stream on port 9999 on localhost  
+            text_stream =  ssc.socketTextStream("localhost", 9999)
+            
+            #Create new streams, splitting them into triples (e.g. preform transformation)
+            googPrice = text_stream.map(lambda line : (line.split(" ")[0], float(line.split(" ")[1])))
+            msftPrice = text_stream.map(lambda line : (line.split(" ")[0], float(line.split(" ")[2])))
 
 
-          findHigherGoog(4, 10)
-          findHigherGoog(20, 10)
+            #Create 10-day and 40-day averages
+            goog10Day = googPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(10, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
+                                      .filter(lambda x: x[2] == 10)\
+                                      .map(lambda line: (line[0], line[1]/10, line[2]))
+
+            goog40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(40, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
+                                      .filter(lambda x: x[2] == 40)\
+                                      .map(lambda line: (line[0], line[1]/40, line[2]))
+
+            msft10Day = msftPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(10, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
+                                      .filter(lambda x: x[2] == 10)\
+                                      .map(lambda line: (line[0], line[1]/10, line[2]))
+            msft40Day = msftPrice.map(lambda line: (line[0], line[1], 1))\
+                                      .window(40, 1)\
+                                      .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
+                                      .filter(lambda x: x[2] == 40)\
+                                      .map(lambda line: (line[0], line[1]/40, line[2]))
 
 
 
+     findHigherGoog(2, 4)
+     findHigherGoog(5, 1)    
+          
