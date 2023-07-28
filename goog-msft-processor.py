a2bed13ba299d1dc6c949fa42ffe5628fc44b7e9
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 03:18:12 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
             signalGoog = goog10Day.join(goog40Day)\
                                     .map(lambda x: (x[0], x[1][0],  x[1][1], findHigher(x[1][0], x[1][1]), findHigher(x[1][0], x[1][1])))\
                                     .reduce(lambda a, b: (max(a[0], b[0]), max(a[1], b[1]), max(a[2], b[2]), a[4], b[4]))\
-                                    .filter(lambda x: (x[3] != x[4]))
+                                    #.filter(lambda x: (x[3] != x[4]))
                                     #.map(lambda x: (x[0], x[1]))
 
             signalMsft = msft10Day.join(msft40Day)\
