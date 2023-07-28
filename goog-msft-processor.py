fa5d8fd497e0187d63e4b78fad089c4abff4c101
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 14:21:51 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -48,16 +48,16 @@ if __name__ == "__main__":
 
             #Create 10-day and 40-day averages
             goog10Day = googPrice.map(lambda line: (line[0], line[1], 1))\
-                                      .window(3, 1)\
+                                      .window(10, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
-                                      .filter(lambda x: x[2] == 3)\
-                                      .map(lambda line: (line[0], line[1]/3, line[2]))
+                                      .filter(lambda x: x[2] == 10)\
+                                      .map(lambda line: (line[0], line[1]/10, line[2]))
 
             goog40Day = googPrice.map(lambda line: (line[0], line[1], 1))\
-                                      .window(4, 1)\
+                                      .window(40, 1)\
                                       .reduce(lambda a, b: (max(a[0], b[0]), a[1] + b[1], a[2] + b[2]))\
-                                      .filter(lambda x: x[2] == 4)\
-                                      .map(lambda line: (line[0], line[1]/4, line[2]))
+                                      .filter(lambda x: x[2] == 40)\
+                                      .map(lambda line: (line[0], line[1]/40, line[2]))
 
             msft10Day = msftPrice.map(lambda line: (line[0], line[1], 1))\
                                       .window(10, 1)\
