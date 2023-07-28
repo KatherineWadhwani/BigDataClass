29913546c06d2f9ec4f63dcddfe8286a9f822064
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 14:05:40 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -21,7 +21,21 @@ if __name__ == "__main__":
             ssc = StreamingContext(sc, 1)
 
             def signal(d1, d2):
-                        return "starting to work"
+                        trend 1 = "null"
+                        trend 2 = "null"
+                        if (d1[1] > d1[2]):
+                                    trend 1 = "tenDay"
+                        if (d1[1] < d1[2]):
+                                    trend 1 = "fortyDay"
+                        if (d2[1] > d2[2]):
+                                    trend 2 = "tenDay"
+                        if (d2[1] < d2[2]):
+                                    trend 2 = "fortyDay"
+                        if (trend 1 == "tenDay" && trend 2 == "fortyDay"):
+                                    return str(d2[0]) + "sell goog"
+                        if (trend 1 == "fortyDay" && trend 2 == "tenDay"):
+                                    return str(d2[0]) + "buy goog"
+                        return "null"
 
            
             #Create stream on port 9999 on localhost  
