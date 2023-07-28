ab296e09a609fb06df8abb1e5cd91add62ba5238
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-28 12:29:09 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
             #msft10Day.pprint()
             #msft40Day.pprint()
                                          
-            googRecs.pprint()
+            signalGoog.pprint()
             #signalMsft.pprint()
             
             #Run
