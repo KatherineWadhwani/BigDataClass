b737dcf91a6b0c843392a344deae29e0a5cf0283
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-27 23:45:26 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
                                     if (oldTop == topGoog):
                                                 return "1"
                                     if (oldTop != topGoog and oldTop == "empty"):
-                                                return topGoog
+                                                return "2"
                                     if (oldTop != topGoog and oldTop != "empty"):
                                                 return "buy "
                         else:
