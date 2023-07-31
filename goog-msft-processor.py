23c855ddca087401ee78e6e4c0bca1a50696668a
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 02:12:17 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -129,7 +129,16 @@ if __name__ == "__main__":
                         clean_sents(review)
                         data_words = list(sent_to_words(data))
                         data_words = [dw for dw in data_words if len(dw)>0]
-                        print(data_words)
+                        # Build the bigram and trigram models
+                        bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+                        trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+                        
+                        # Faster way to get a sentence clubbed as a trigram/bigram
+                        bigram_mod = gensim.models.phrases.Phraser(bigram)
+                        trigram_mod = gensim.models.phrases.Phraser(trigram)
+                        
+                        # See trigram example
+                        print(trigram_mod[bigram_mod[data_words[0]]])
             
        
             
