b6be7f5450bfbea86c49c59c027eab5e0bf70c4e
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 01:20:00 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -134,7 +134,8 @@ if __name__ == "__main__":
             reviewsDF = pd.read_csv('reviews.csv', names=colnames)
             
             for review in reviewsDF.ReviewText:
-                        clean_sents(review)
+                        a = "dfasdf"
+                        clean_sents(a)
             
        
             
