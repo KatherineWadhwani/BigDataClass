a1d8fcd0820aaa713d7ed92b8bc1c8853bcd9627
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-30 21:37:07 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -47,6 +47,6 @@ if __name__ == "__main__":
                         for j in files[i].readlines():
                                     j.lower()
                         
-                        file.readlines()
+                        files[i].readlines()
 
 
