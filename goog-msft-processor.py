f363705fc6e4b50e9545d51332005dfc27eff5b9
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 03:18:51 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
             def clean_sents(data):
                         # Remove new line characters
                         data = re.sub('\s+', ' ', str(data))
-                        data = re.sub('[^0-9a-zA-Z]+', '', str(data))
+                        data = re.sub('[^0-9a-zA-Z]', '', str(data))
                         return data
                         
             def sent_to_words(sentences):
