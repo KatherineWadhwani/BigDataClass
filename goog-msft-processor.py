5583c99698f999999da9525fb1bd7be740634506
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 03:49:41 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -201,7 +201,6 @@ if __name__ == "__main__":
                                                                    per_word_topics=True)
                         #print(lda_model.print_topics())
                         doc_lda = lda_model[corpus]
-                        pyLDAvis.enable_notebook()
                         vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
                         vis
                                                 
