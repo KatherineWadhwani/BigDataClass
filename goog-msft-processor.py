9f6fc5fa90cb2649d8b77307c71db870f7971b3f
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 02:18:21 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -128,6 +128,7 @@ if __name__ == "__main__":
             for review in reviewsDF.ReviewText:
                         review = clean_sents(review)
                         data_words = list(sent_to_words(review))
+                        print(data_words)
                         data_words = [dw for dw in data_words if len(dw)>0]
                         # Build the bigram and trigram models
                         bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
@@ -138,7 +139,6 @@ if __name__ == "__main__":
                         trigram_mod = gensim.models.phrases.Phraser(trigram)
                         
                         # See trigram example
-                        print(data_words)
                         #print(trigram_mod[bigram_mod[data_words[0]]])
             
        
