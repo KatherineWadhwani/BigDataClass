272d31a26319478beb3fcdee2dc18c2d60fbc050
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 12:16:11 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
                         #print ([[(id2word[id], freq) for id, freq in cp] for cp in corpus])
 
                         num_topics = 1
-                        print(corpus)
+                        #print(corpus)
                         lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                                                    id2word=id2word,
                                                                    num_topics=num_topics, 
