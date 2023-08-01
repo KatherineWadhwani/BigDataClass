7d171260847d7e1a996e2b4afc15624f328cd86b
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-07-31 21:35:04 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -6,39 +6,14 @@ import math
 import string
 import re
 import pprint
-import matplotlib.pyplot as plt
-import seaborn as sns
 import nltk
 import csv
-import pyLDAvis
-import pyLDAvis.gensim 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark import SparkFiles
 from pyspark.sql import SQLContext
 from pyspark.sql import Row
 from pyspark.sql import SparkSession
-from nltk.corpus.reader.util import StreamBackedCorpusView
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.data import load
-from nltk.corpus import stopwords
-nltk.download('punkt')
-nltk.download('tagsets')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-import gensim
-from gensim import corpora
-from gensim.utils import simple_preprocess
-from gensim.models import CoherenceModel
-os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
-os.environ["SPARK_HOME"] = "spark-3.2.1-bin-hadoop3.2"
-import spacy
-spacy.load('en_core_web_sm')
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
-import warnings
-warnings.filterwarnings("ignore",category=DeprecationWarning)
-stop_words = stopwords.words('english')
 
 
 #Setup 
