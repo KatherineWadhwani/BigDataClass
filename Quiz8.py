3a21ad40438ef12173a18939d80a31857003e8be
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-01 00:31:34 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -15,8 +15,22 @@ from pyspark.sql import SQLContext
 from pyspark.sql import Row
 from pyspark.sql import SparkSession
 
-
+#does order matter or can use set?
 #Setup 
+def hash1(num1, num2):
+  M = 2^13 - 1
+  print ((i*100 + 1) % M)
+  
+
+def computeJacc(num1, num2):
+  list1 = reviews[num1]
+  list2 = reviews[num2]
+  intersect = len(list1.intersection(list2))
+  union = len(list1.union(list2))
+  jaccard = (intersect/union)
+  if (jaccard > 0.5):
+    print("Users " + str(num1) + " and " + str(num2) + " have a Jaccard similarity of " + str(jaccard))
+
 f1 = pd.read_csv('ml-latest-small/movies.csv')
 f2 = pd.read_csv('ml-latest-small/ratings.csv')
 out = pd.merge(f1,f2,on='movieId',how='inner')
@@ -29,15 +43,6 @@ for num in range(611):
   list = set()
   reviews[num] = list
 
-def computeJacc(num1, num2):
-  list1 = reviews[num1]
-  list2 = reviews[num2]
-  intersect = len(list1.intersection(list2))
-  union = len(list1.union(list2))
-  jaccard = (intersect/union)
-  if (jaccard > 0.5):
-    print("Users " + str(num1) + " and " + str(num2) + " have a Jaccard similarity of " + str(jaccard))
-
 for i in range(len(out)):
   list = reviews.get(out.loc[i, "userId"])
   list.add(out.loc[i, "movieId"])
@@ -46,5 +51,9 @@ for num1 in range(611):
   for num2 in range(611):
     if (num1 != num2):
       computeJacc(num1, num2)
+      hash1(num1, num2)
+
+
+
 
 
