115751e5d15f8d12672dd2e7e76782c34fafb468
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-01 01:28:40 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -77,7 +77,7 @@ for i in range(len(out)):
 for num in range(611):
   for i in range(193609):
     if (i in reviewsJaccard[num]):
-      reviewsMinHash[i] = 1
+      reviewsMinHash[num][i] = 1
     print(reviewsMinHash[num])
 
   
