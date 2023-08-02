0a9b7f80fb35b1b684081f2b5d9d38cb240af17a
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-01 18:07:29 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -62,7 +62,8 @@ if __name__ == "__main__":
             #print(all_tagged)
             #Print dict
             tagdict = load('help/tagsets/upenn_tagset.pickle')
-            for i in range(len(all_tagged)):
+            print(len(all_tagged))
+            for i in range(len(all_tagged[0])):
             	tagType = all_tagged[0][i][1]
             	if ((tagType == "JJ" or tagType == "JJR" or tagType == "JJS") and adjCount < 10):
             		adjs[adjCount] = all_tagged[0][i][0]
