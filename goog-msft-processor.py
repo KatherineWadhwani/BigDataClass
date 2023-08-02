761e6390f6771cd011d0fca39f1c306f221522d7
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-01 18:38:12 -0700

    Update goog-msft-processor.py

--- goog-msft-processor.py
+++ goog-msft-processor.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
 	
             #Print word types
             for j in range(len(all_tagged)):
-	            for i in range(len(all_tagged[j])):
-			print(all_tagged[i][j])
+            	for i in range(len(all_tagged[j])):
+	        	print(all_tagged[i][j])
 	            	"""tagType = all_tagged[0][i][1]
 	            	if ((tagType == "JJ" or tagType == "JJR" or tagType == "JJS") and adjCount < 10):
 	            		adjs[adjCount] = all_tagged[0][i][0]
