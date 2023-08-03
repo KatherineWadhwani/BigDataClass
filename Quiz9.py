f76fbb19ae1e37552be4f13b6822908f9b541bbb
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 14:40:13 -0700

    Update Quiz9.py

--- Quiz9.py
+++ Quiz9.py
@@ -150,7 +150,7 @@ reviewsDF = pd.read_csv('reviews.csv', names=colnames)
 		
 reviewsDict = {reviewsDF.loc[row, 'recNo']: reviewsDF.loc[row, 'ReviewText'] for row in range (len(reviewsDF))}
 
-keys[] = reviewsDict.keys()
+keys = reviewsDict.keys()
 for key in keys:
 	if (isinstance(reviewsDict[key], float)):
 		reviewsDict.pop(key)
