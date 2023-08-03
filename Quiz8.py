a4d41b915d9abf1690963c331c40fe8c194334e5
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 02:04:54 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -23,7 +23,7 @@ def hash(hashNum):
       M = 2^13 - 1
       movie = f1.loc[((3*hashNum + 1) % M, "movieCount")]
       if(movie in reviewsJaccard[num]):
-        print(reviewsMinHash[num])
+        print(num)
         list = reviewsMinHash[num]
         list.append(movie)
         reviewsMinHash[num] = list
