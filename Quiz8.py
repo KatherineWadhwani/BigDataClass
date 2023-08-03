672a90bf715498a74b1872dd90b0208c80871702
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 01:51:08 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -25,7 +25,7 @@ def hash(hashNum):
       for integer in range(9742):
         M = 2^13 - 1
         movie = f1.loc[((integer*hashNum + 1) % M, "movieCount")]
-        print((integer*hashNum + 1) % M)
+        print((integer*hashNum + 1))
         """if(movie in reviewsJaccard[num]):
               reviewsMinHash[integer][hashNum] = movie
               break"""
