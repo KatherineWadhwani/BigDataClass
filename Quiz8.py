054259c9b996c8493ae7bafabf80e00512ad248b
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 03:34:36 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -73,6 +73,11 @@ for i in range(len(out)):
   list = reviewsJaccard.get(out.loc[i, "userId"])
   list.append(out.loc[i, "movieId"])
   reviewsJaccard[out.loc[i, "userId"]] = list
+
+for num1 in range(611):
+  for num2 in range(611):
+    if (num1 != num2 and num1 < num2):
+      computeJacc(num1, num2)
   
 for num in range(1, 51):
   hash(num)
