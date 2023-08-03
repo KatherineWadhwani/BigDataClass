ca7561101b3cd06d6f5addf2b7ae3cdada625f69
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 15:08:46 -0700

    Update Quiz9.py

--- Quiz9.py
+++ Quiz9.py
@@ -205,8 +205,8 @@ for key in newDict.keys():
 	#print(speeches_corpus)
 
 	num_topics = 10
-	#print(corpus)
-	#print(len(corpus))
+	print(corpus)
+	print(len(corpus))
 	lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=num_topics, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
 	#print(lda_model.print_topics())
         #doc_lda = lda_model[corpus]
