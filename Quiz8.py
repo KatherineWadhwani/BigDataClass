b3dc737f11e75019f52f10ff164e504a7a82b3a8
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 03:18:03 -0700

    Update Quiz8.py

--- Quiz8.py
+++ Quiz8.py
@@ -18,17 +18,17 @@ from pyspark.sql import SparkSession
 #does order matter or can use set?
 #Setup 
 def hash(hashNum):
-  for num in range(3):
+  for num in range(611):
       M = 2^13 - 1
       for integer in range(9742):
         movie = f1.loc[((integer*hashNum + 1) % M, "movieCount")]
+        print(movie)
         if(movie in reviewsJaccard[num]):
-          print(num)
+          #print(num)
           list = reviewsMinHash[num]
           list.append(movie)
           reviewsMinHash[num] = list
           break
-        print(movie)
 
 def computeJacc(num1, num2):
   set1 = set(reviewsJaccard[num1])
