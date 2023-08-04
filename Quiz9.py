d54341de6b82cd1fa11a4732017537e8eff47da1
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 20:58:04 -0700

    Update Quiz9.py

--- Quiz9.py
+++ Quiz9.py
@@ -195,7 +195,7 @@ corpus = [id2word.doc2bow(text) for text in texts]
 speeches_corpus = dict(id2word)
 #print(speeches_corpus)
 
-num_topics = 10
+num_topics = 1
 #print(corpus)
 #print(len(corpus))
 lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=num_topics, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
