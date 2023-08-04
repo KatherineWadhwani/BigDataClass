3439599084a03f1010e3e30046e5091de6f390ca
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 20:52:48 -0700

    Update Quiz9.py

--- Quiz9.py
+++ Quiz9.py
@@ -141,7 +141,6 @@ def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
     for sent in texts:
         doc = nlp(" ".join(sent)) 
         texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
-        print("here")
     return texts_out
                                                
 
