aed2121e591c28ac1cd610d0040d65d943b25619
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 21:04:01 -0700

    Update Quiz9.py

--- Quiz9.py
+++ Quiz9.py
@@ -195,10 +195,10 @@ corpus = [id2word.doc2bow(text) for text in texts]
 speeches_corpus = dict(id2word)
 #print(speeches_corpus)
 
-num_topics = 1
+num_topics = 10
 #print(corpus)
 #print(len(corpus))
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=num_topics, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=num_topics, random_state=100, update_every=1, chunksize=100, passes=1, alpha='auto', per_word_topics=True)
 print(lda_model.print_topics())
 #doc_lda = lda_model[corpus]
 #https://stackoverflow.com/questions/40840731/valueerror-cannot-compute-lda-over-an-empty-collection-no-terms
