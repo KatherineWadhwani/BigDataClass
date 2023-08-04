21971f72f2896b4f389fc8ee2002d983601ca661
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-08-03 18:07:06 -0700

    Update Quiz9.py

--- Quiz9.py
+++ Quiz9.py
@@ -156,7 +156,7 @@ reviewsDict = {reviewsDF.loc[row, 'recNo']: reviewsDF.loc[row, 'ReviewText'] for
 speechesDF = pd.DataFrame(reviewsDict.items(), columns=['recNo', 'ReviewText'])
 #print(speechesDF)
 
-for speech in len(speechesDF):
+for speech in range(len(speechesDF)):
     content = speechesDF.loc[speech, "ReviewText"][:1]
     contentList = content.to_list()
     print(contentList[0])
